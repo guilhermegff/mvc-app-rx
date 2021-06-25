7e9fbc86b547a5c6cdd41593e3720b07f37a6cc1
Guilherme <tguilhermegff@fastshop.com.br>
2021-06-25 14:41:24 -0300

    Implementation of service related classes

---
+++ apis/hotmartservice/src/main/java/com/project/hotmartservice/HotMartApi.kt
@@ -0,0 +1,47 @@
+package com.project.hotmartservice
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import com.project.hotmartservice.model.Locations
+import io.reactivex.Single
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "http://hotmart-mobile-app.herokuapp.com"
+
+object HotMartApi : HotMartDataSource {
+
+    private val service : HotMartService
+
+    init {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request()
+                chain.proceed(request)
+            }
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+        val gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.IDENTITY)
+            .setPrettyPrinting()
+            .create()
+
+        val retrofit = Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        service = retrofit.create(HotMartService::class.java)
+    }
+
+    override fun stores() : Single<Locations> = service.stores()
+}
