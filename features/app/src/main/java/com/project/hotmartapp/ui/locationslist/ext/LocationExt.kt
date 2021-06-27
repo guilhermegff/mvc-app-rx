4e003bc3ead7c4c05b06887a1ca7b167e04d69a5
Guilherme <tguilhermegff@fastshop.com.br>
2021-06-26 23:26:21 -0300

    Changed the item layout and related classes

--- features/app/src/main/java/com/project/hotmartapp/ui/locationslist/ext/LocationExt.kt
+++ features/app/src/main/java/com/project/hotmartapp/ui/locationslist/ext/LocationExt.kt
@@ -3,4 +3,4 @@ package com.project.hotmartapp.ui.locationslist.ext
 import com.project.hotmartapp.ui.locationslist.component.LocationViewItem
 import com.project.hotmartservice.model.Location
 
-fun Location.toLocationViewItem() = LocationViewItem(id, type, "", rating, "")
+fun Location.toLocationViewItem() = LocationViewItem(id, "", name, type, rating, "")
