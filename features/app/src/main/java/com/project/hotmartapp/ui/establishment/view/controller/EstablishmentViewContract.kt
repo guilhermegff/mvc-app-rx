7e41713810a563a8ad48526e0f664ceaf7ab8494
Guilherme <tguilhermegff@fastshop.com.br>
2021-06-28 19:19:45 -0300

    Made a refactor on the xml file, added fun to contract

--- features/app/src/main/java/com/project/hotmartapp/ui/establishment/view/controller/EstablishmentViewContract.kt
+++ features/app/src/main/java/com/project/hotmartapp/ui/establishment/view/controller/EstablishmentViewContract.kt
@@ -2,7 +2,9 @@ package com.project.hotmartapp.ui.establishment.view.controller
 
 import com.project.hotmartapp.core.BaseListener
 import com.project.hotmartapp.core.ObservableViewContract
+import com.project.hotmartservice.model.Establishment
 
 interface EstablishmentViewContract : ObservableViewContract<EstablishmentViewContract.Listener> {
     interface Listener : BaseListener
+    fun showEstablishment(establishment: Establishment)
 }
